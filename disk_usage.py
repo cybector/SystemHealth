36b385fca6e49703412e6b69c31cdb2fcf5918f4
cybector <tylersphotography2018@gmail.com>
2023-06-03 01:05:17 -0400

    added disk_usage module

--- disk_usage.py
+++ disk_usage.py
@@ -1,7 +1,12 @@
+"""
+This module provides a function for retrieving disk usage information.
+"""
 import shutil
 
 
-# Function to get disk usage statistics
 def get_disk_usage(path):
+    """
+    Returns the total, used, and free disk space for the given path.
+    """
     total, used, free = shutil.disk_usage(path)
     return total, used, free
