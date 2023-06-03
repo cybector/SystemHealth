bdf78151a8f9839cc37c43b2beb35dba4bfcb513
cybector <tylersphotography2018@gmail.com>
2023-06-03 01:04:19 -0400

    refactored main module

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from system_info import *
-from disk_usage import get_disk_usage
+"""
+This is the main module that utilizes functions from system_info to print out system information.
+"""
+from system_info import (
+    get_os_info,
+    get_cpu_info,
+    get_memory_info,
+    get_disk_info,
+    get_network_info,
+)
 
 
-# Main function to print system information
 def main():
-    path = "C:\\"
-    total, used, free = get_disk_usage(path)
-    print(f"Disk Usage Statistics for drive {path}")
-    print(f"Total: {total // (2**30)} GiB")
-    print(f"Used: {used // (2**30)} GiB")
-    print(f"Free: {free // (2**30)} GiB")
-    print(f"\nCPU usage: {get_cpu_usage()}%")
-    print(f"\nMemory usage: {get_memory_usage()}%")
-    print("\nRunning processes:")
-    for proc in get_running_processes():
-        print(proc)
-    hostname, ip_address = get_network_info()
-    print(f"\nNetwork info: Hostname: {hostname}, IP Address: {ip_address}")
-    print(f"\nOperating System info: {get_os_info()}")
-    print(f"\nLast system boot time: {get_boot_time()}")
+    """
+    Prints out system information using functions from system_info module.
+    """
+    print("Operating System Information: ", get_os_info())
+    print("CPU Information: ", get_cpu_info())
+    print("Memory Information: ", get_memory_info())
+    print("Disk Information: ", get_disk_info())
+    print("Network Information: ", get_network_info())
 
 
 if __name__ == "__main__":
